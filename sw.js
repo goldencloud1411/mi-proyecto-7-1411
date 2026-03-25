6d5029bccabdefce71a4876a0de09b651117a75c
goldencloud1411 <ritavaleria22@gmail.com>
2026-03-25 18:12:28 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = 'army-schedule-v2';
-const assets = ['./', './index.html', './script.js', './icon.png', './arirang logo.png'];
+const CACHE_NAME = 'army-schedule-v3';
+const assets = [
+  './',
+  './index.html',
+  './script.js',
+  './icon.png',
+  './arirang album.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
